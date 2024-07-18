14add7df9933d3677451e3e4cb97147eea016ea8
Crawford Wynnes <crawfordwynnes@gmail.com>
2024-07-18 12:36:54 +0100

    Make comment more readable so that it does not look like it's about the abstract class

--- Bicycle.rb
+++ Bicycle.rb
@@ -1,7 +1,7 @@
 # Show's that certain subclasses must implement certain methods
 #
 # This code illustrates that a superclass can be instatiated and used although the idea would be to use sub classes with single responsibility
-# i.e a bicycle can be instantiated to use a class with a smaller external api.
+# i.e a bicycle can be instantiated as opposed to a Mountain bike to use a class with a smaller external api.
 #
 class Bicycle < AbstractBicycle
   def initialize(args)
